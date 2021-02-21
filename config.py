4ef5abcf93f382812417bf4155d8d0a141bc6e6a
readme9txt <i@remilia-scarlet.red>
2021-02-21 22:55:16 +0800

    更改为绝对路径

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import configparser
 import os
 
 # 加载配置文件
-ini_file = 'configs.ini'
+current_path = os.path.abspath(os.path.dirname(__file__))
+ini_file = '{}/configs.ini'.format(current_path)
 conf = configparser.ConfigParser()
 if not os.path.exists(ini_file):
     conf.add_section('config')
-    conf.set('config', 'output_dir', './')
+    conf.set('config', 'output_dir', current_path)
     conf.write(open(ini_file, 'w'))
 conf.read(ini_file)
 
@@ -22,4 +23,4 @@ class Config:
 
 
 if not os.path.exists(Config.output_dir):
-    Config.output_dir = './'
+    Config.output_dir = current_path
